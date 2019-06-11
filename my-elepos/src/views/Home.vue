d082adb89556845af808dee6862110e769df6341
MrZhang <332004976@qq.com>
2019-06-11 23:14:10 +0800

    完成主页左侧订单信息展展示

--- my-elepos/src/views/Home.vue
+++ my-elepos/src/views/Home.vue
@@ -1,10 +1,35 @@
 <template>
   <div class="hello">
+    <el-row>
+      <el-col :span="7" class="pos-order" id="order-list">
+        <el-tabs>
+          <el-tab-pane label="点餐">
+            <el-table>
+              <el-table-column prop="goodsName" label="商品名称"></el-table-column>
+              <el-table-column prop="count" label="数量"></el-table-column>
+              <el-table-column prop="price" label="金额"></el-table-column>
+              <el-table-column label="操作"></el-table-column>
+            </el-table>
+          </el-tab-pane>
+          <el-tab-pane label="挂单">挂单</el-tab-pane>
+          <el-tab-pane label="外卖">外卖</el-tab-pane>
+        </el-tabs>
+      </el-col>
+      <el-col>
+        产品栏
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
 export default {
+  name: 'hello',
+  mounted() {
+    var orderH = document.body.clientHeight;
+    document.getElementById('order-list').style.height = orderH + "px";
+    document.getElementById('order-list').style.paddingLeft = 5 +"px";
+  },
 }
 </script>
 
