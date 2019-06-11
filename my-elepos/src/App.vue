d3fb796e6b6b9ec531d2b16e8676d1c1e1be727d
MrZhang <332004976@qq.com>
2019-06-11 21:57:17 +0800

    完成左边导航栏

---
+++ my-elepos/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Leftnav/>
+    <div class="main">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Leftnav from './components/mini/Leftnav.vue'
+export default {
+  name: "app",
+  components: {
+    Leftnav
+  }
+};
+</script>
+
+<style>
+.main{
+  float: left;
+  width: 95%;
+  background-color: #eff2f7;
+  height: 100%;
+  overflow: hidden
+}
+</style>
---
+++ my-elepos/src/components/mini/Leftnav.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="left-nav">
+        <ul>
+            <li>
+                <i class="icon iconfont icon-shouyin"></i>
+                <div>收银</div>
+            </li>
+            <li>
+                <i class="icon iconfont icon-dianpu"></i>
+                <div>店铺</div>
+            </li>
+            <li>
+                <i class="icon iconfont icon-icon-test"></i>
+                <div>商品</div>
+            </li>
+            <li>
+                <i class="icon iconfont icon-Member"></i>
+                <div>会员</div>
+            </li>
+            <li>
+                <i class="icon iconfont icon-count"></i>
+                <div>统计</div>
+            </li>
+            <li>
+                <i class="icon iconfont icon-shezhi"></i>
+                <div>设置</div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+};
+</script>
+<style scoped>
+.left-nav{
+    color: #fff;
+    font-size: 10px;
+    height: 100%;
+    background-color: #1D8CE0;
+    float: left;
+    width: 5%
+}
+.iconfont{
+    font-size: 24px
+}
+.left-nav ul{
+    padding: 0px;
+    margin: 0px
+}
+.left-nav li{
+    list-style: none;
+    text-align: center;
+    border-bottom: 1px solid #58bc58;
+    padding: 10px;
+}
+</style>
